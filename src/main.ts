cda867aa33977c5dcafb771e725bb27e9ba03f75
Mahdi Pishguy <pishguy@gmail.com>
2021-08-12 21:59:48 +0430

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,12 @@ import {Logger} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {TypeOrmModule} from '@nestjs/typeorm';
-const port = process.env.PORT || 3000;
+const port = 4000;//process.env.PORT || 4000;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(port);
     Logger.log(`server started on ${port}`, 'Bootstrap');
-
 }
 
 bootstrap();
